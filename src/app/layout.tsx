b6cbabf087bc4a6feb5408e4be383fdb66c19f3a
Navnit73 <navnitrai5389@gmail.com>
2026-08-24 19:43:40 +0530

    chore: update Microsoft Clarity project ID in layout script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "xrutgigzx0");
+            })(window, document, "clarity", "script", "y7f6cnkwxb");
           `}
         </Script>
       </head>
